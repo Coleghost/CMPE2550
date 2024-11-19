807aa9c4661da73b040e59b18517252a8611c1e8
Cole Ghostkeeper <coleghost1998@gmail.com>
2024-11-18 20:25:45 -0700

    commit

--- icas/coleghost_ica08/coleghost_ica08/Program.cs
+++ icas/coleghost_ica08/coleghost_ica08/Program.cs
@@ -1,13 +1,20 @@
 namespace coleghost_ica08
 {
+    using Microsoft.Data.SqlClient;
     public class Program
     {
         public static void Main(string[] args)
         {
+            // set up builder and controllers
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
             var app = builder.Build();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed(origin => true));
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => "COLE GHOSTKEEPER ICA08");
+
+            // define the connection string
+            string connectionString = "Server = data.cnt.sast.ca,24680; Database = demoUser_ClassTrak; User Id = demoUser; Password = temP2020#; Encrypt = False;";
 
             app.Run();
         }

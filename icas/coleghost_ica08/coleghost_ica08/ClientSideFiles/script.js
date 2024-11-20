706935ba5745b789a18080b0703dea2b56a07d6a
Cole Ghostkeeper <coleghost1998@gmail.com>
2024-11-20 14:52:33 -0700

    commit

---
+++ icas/coleghost_ica08/coleghost_ica08/ClientSideFiles/script.js
@@ -0,0 +1,22 @@
+
+window.onload = function(){
+
+}
+
+// Use this function to make an ajax call
+function AjaxRequest(url, type, data, dataType, successFunc, errorFunc){
+    let ajaxOptions = {};
+    ajaxOptions.url = url;
+    ajaxOptions.type = type;
+    ajaxOptions.data = JSON.stringify(data); // asp ajax call
+    ajaxOptions.dataType = dataType;
+    ajaxOptions.success = successFunc;
+    ajaxOptions.error = errorFunc;
+    ajaxOptions.contentType = "application/json";
+
+    $.ajax(ajaxOptions); // make the ajax call and return the value
+}
+
+function ErrorHandler(request, status, error){
+    console.log("Ajax request invalid");
+}
--- icas/coleghost_ica08/coleghost_ica08/Program.cs
+++ icas/coleghost_ica08/coleghost_ica08/Program.cs
@@ -1,6 +1,7 @@
+using Microsoft.Data.SqlClient;
+
 namespace coleghost_ica08
 {
-    using Microsoft.Data.SqlClient;
     public class Program
     {
         public static void Main(string[] args)
@@ -16,6 +17,26 @@ namespace coleghost_ica08
             // define the connection string
             string connectionString = "Server = data.cnt.sast.ca,24680; Database = demoUser_ClassTrak; User Id = demoUser; Password = temP2020#; Encrypt = False;";
 
+            app.MapGet("/retrieveData",() =>
+            {
+                // Step 1: Establish a connection to DB
+                // Pass connection string here
+                // Use SQLConnection class to open a connection to the db.
+                SqlConnection connection = new SqlConnection(connectionString);
+                // Step 2: Open the connection
+                connection.Open();
+                // Step 3:  Prepare your query
+                string query = "SELECT * FROM Students WHERE first_name = E% OR first_name = F%";
+                // step 4: execute SQL
+                SqlCommand command = new SqlCommand(query, connection);
+                // step 5: run query
+                SqlDataReader reader = command.ExecuteReader();
+                
+                while (reader.Read())
+                {
+
+                }
+            });
             app.Run();
         }
     }
